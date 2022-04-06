a0bf987efdd33317cb65b070b4bee32ec835dab7
Lakshmid24 <100596113+Lakshmid24@users.noreply.github.com>
2022-04-06 19:54:56 +0530

    Add files via upload

---
+++ Assignment 3 program 2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+# In[1]:
+
+
+def tripple(n):
+    return 3*n
+x=[1,2,3,4,5,6,7]
+t=map(tripple,x)
+print("Triple of  list numbers: ")
+print(list(t))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
